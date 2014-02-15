342e8a4d07319de4591d6b2a06e11bdc95a8b0db
benoitMariaux <benoit.mariaux@gmail.com>
2014-02-15 16:47:49 +0100

    more

---
+++ Tests/BreadcrumbManagerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BCM\BreadcrumbBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BreadcrumbManagerTest extends WebTestCase
+{
+    /**
+     * Todo
+     */
+    public function testBr()
+    {
+        $this->assertTrue(1 == 1);
+        $this->assertFalse(2 == 1);
+    }
+}
